07bb5e2e48c14c3e73a9d880e7c7a0f321230eb5
Gary Odendaal <godendaal@plusonex.com>
2022-11-09 08:55:54 +0200

    update dummy data in expense item

--- expenses/src/components/ExpenseItem/ExpenseItem.js
+++ expenses/src/components/ExpenseItem/ExpenseItem.js
@@ -1,5 +1,18 @@
-const ExpenseItem = () => {
-    return <h2>Expense Item</h2>
-}
+import "./ExpenseItem.css";
 
-export default ExpenseItem
+const ExpenseItem = () => {
+  const expenseDate = new Date(2022,2,28);
+  const expenseTitle = "Car Insurance";
+  const expenseValue = 1495.58
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toDateString()}</div>
+      <div className="expense-item__description">
+        <h2 className="expense-item__h2">{expenseTitle}</h2>
+        <div className="expense-item__price">R{expenseValue}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
