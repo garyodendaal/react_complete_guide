a6ba4618a82bc7d06c20c1e1eb3bfe5e1bb3dd25
Gary Odendaal <godendaal@plusonex.com>
2022-11-10 08:07:15 +0200

    add dynamic props to expense item

--- expenses/src/components/ExpenseItem/ExpenseItem.js
+++ expenses/src/components/ExpenseItem/ExpenseItem.js
@@ -1,15 +1,12 @@
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2022,2,28);
-  const expenseTitle = "Car Insurance";
-  const expenseValue = 1495.58
+const ExpenseItem = ({ title, amount, date }) => {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toDateString()}</div>
+      <div>{date.toDateString()}</div>
       <div className="expense-item__description">
-        <h2 className="expense-item__h2">{expenseTitle}</h2>
-        <div className="expense-item__price">R{expenseValue}</div>
+        <h2 className="expense-item__h2">{title}</h2>
+        <div className="expense-item__price">R{amount}</div>
       </div>
     </div>
   );
