f11fadec114cd798a66644fd14bf1770ba3179bb
Gary Odendaal <godendaal@plusonex.com>
2022-11-10 08:19:23 +0200

    move expense date to separate component

--- expenses/src/components/ExpenseItem.js
+++ expenses/src/components/ExpenseItem.js
@@ -1,9 +1,12 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ title, amount, date }) => {
+  
+
   return (
     <div className="expense-item">
-      <div>{date.toDateString()}</div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2 className="expense-item__h2">{title}</h2>
         <div className="expense-item__price">R{amount}</div>
